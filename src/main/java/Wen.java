01b61c9e9e263240bac6d0e6497d4f80548aedf3
yuu <58155530+kagiura@users.noreply.github.com>
2025-02-07 15:38:59 +0800

    add basic initial greeting / terminating msgs

---
+++ src/main/java/Wen.java
@@ -0,0 +1,17 @@
+public class Wen {
+    public static void main(String[] args) {
+        initializeAndGreet();
+        terminateAndGoodbye();
+    }
+
+    private static void initializeAndGreet() {
+        System.out.println("Hello, I'm Wen!");
+        System.out.println("Let me know what I can help you with~☆");
+    }
+
+    private static void terminateAndGoodbye() {
+        System.out.println();
+        System.out.println("Aw, you're already going?");
+        System.out.println("It's okay, let's meet again soon!");
+    }
+}
