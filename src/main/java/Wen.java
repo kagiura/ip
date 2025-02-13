7796838a25f1c430b2e6135898374eeb96aa5193
kagiura <58155530+kagiura@users.noreply.github.com>
2025-02-13 16:01:13 +0800

    add tasks

--- src/main/java/Wen.java
+++ src/main/java/Wen.java
@@ -1,19 +1,34 @@
 import java.util.Scanner;
 
 public class Wen {
+
+    private static int taskCount = 0;
+    private static Task[] tasks = new Task[100];
+
     public static void main(String[] args) {
         initializeAndGreet();
 
-        String command = "";
+        String input = "";
         Scanner in = new Scanner(System.in);
 
-        while (!command.equals("bye")) {
-            command = in.nextLine();
+        while (!input.equals("bye")) {
+            input = in.nextLine();
+            final String command = input.split(" ", 2)[0];
+            final String commandArgs = input.split(" ", 2).length > 1 ? input.split(" ", 2)[1] : "";
+
             switch (command) {
                 case "bye":
                     break;
+
+                case "list":
+                    printTasks();
+                    break;
+
+
+
                 default:
-                    System.out.println(command);
+                    addTask(input);
+//                    System.out.println("Unknown command \""+command+"\"! Double check your message and try running again~");
                     break;
             }
         }
@@ -33,5 +48,21 @@ public class Wen {
         System.out.println("It's okay, let's meet again soon!");
 
         System.exit(0);
+    }
+
+    private static void printTasks() {
+        System.out.println("You currently have " + taskCount + " tasks!");
+        for (int i=0; i<taskCount; i++) {
+            tasks[i].print();
+        }
+    }
+
+    private static void addTask(String description) {
+        tasks[taskCount] = new Task(description);
+
+        System.out.println("Task added succesfully:");
+        tasks[taskCount].print();
+
+        taskCount++;
     }
 }
